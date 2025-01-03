cf86f3f1cfcc08472d55bc716af0a5e42a5bd0c6
Praveen Aththanayake <aththanayakempa@gmail.com>
2025-01-04 01:00:04 +0530

    feat: create category crud controllers

---
+++ server/controllers/Category.controller.ts
@@ -0,0 +1,88 @@
+import { Request, Response } from "express";
+import { Category } from "../models";
+import { CreateCategoryInput } from "../dto";
+
+// FindCategory function
+export const FindCategory = async (id: string | undefined, name?: string) => {
+  if (name) {
+    return await Category.findOne({ name: name });
+  } else {
+    return await Category.findById(id);
+  }
+};
+
+// Create Category function
+export const CreateCategory = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { name, slug } = <CreateCategoryInput>req.body;
+
+  const existingCategory = await FindCategory("", name);
+
+  if (existingCategory) {
+    res.status(400).json({ message: "Category already exists" });
+    return;
+  }
+
+  const createCategory = await Category.create({
+    name: name,
+    slug: slug,
+  });
+
+  res.json(createCategory);
+};
+
+// Get Categories function
+export const GetCategories = async (req: Request, res: Response) => {
+  const categories = await Category.find();
+
+  if (categories != null) {
+    res.json(categories);
+  } else {
+    res.json({ message: "No categories found" });
+  }
+};
+
+// Get Category by ID function
+export const GetCategoryById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const category = await FindCategory(id);
+
+  if (category != null) {
+    res.json(category);
+  } else {
+    res.json({ message: "Category not found" });
+  }
+};
+
+// Update Category function
+export const UpdateCategory = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { name, slug } = <CreateCategoryInput>req.body;
+
+  const category = await FindCategory(id);
+
+  if (category != null) {
+    category.name = name;
+    category.slug = slug;
+
+    await category.save();
+    res.json(category);
+  } else {
+    res.json({ message: "Category not found" });
+  }
+};
+
+// Delete Category function
+export const DeleteCategory = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const category = await FindCategory(id);
+
+  if (category != null) {
+    await Category.deleteOne({ _id: id });
+    res.json({ message: "Category deleted" });
+  } else {
+    res.json({ message: "Category not found" });
+  }
+};
