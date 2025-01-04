336d1f41b642b6718e83b5c45458d9d4776c07e4
Praveen Aththanayake <aththanayakempa@gmail.com>
2025-01-05 02:56:37 +0530

    feat: update user routes

--- server/routes/user.routes.ts
+++ server/routes/user.routes.ts
@@ -1,14 +1,35 @@
-import express, { Request, Response, NextFunction } from "express";
-import { CreateUser, GetUserById, GetUsers, UserLogin } from "../controllers";
+import express from "express";
+import {
+  CreateUser,
+  GetUserById,
+  GetUsers,
+  UserLogin,
+  GetUserProfile,
+  UpdateUserProfile,
+  DeleteUser,
+} from "../controllers";
 
 const router = express.Router();
 
+// User Registration
 router.post("/register", CreateUser);
 
+// User Login
 router.post("/login", UserLogin);
 
+// Fetch All Users
 router.get("/users", GetUsers);
 
+// Fetch User by ID
 router.get("/users/:id", GetUserById);
+
+// Get Current User Profile (Protected Route)
+router.get("/profile", GetUserProfile);
+
+// Update User Profile (Protected Route)
+router.put("/profile", UpdateUserProfile);
+
+// Delete Current User (Protected Route)
+router.delete("/profile", DeleteUser);
 
 export { router as UserRoute };
