79a93bb78deafe57a919cedd983cb0de3b8230fd
Praveen Aththanayake <aththanayakempa@gmail.com>
2025-01-05 19:17:22 +0530

    fix: services import issue fixed

--- client/services/index.ts
+++ client/services/index.ts
@@ -2,3 +2,5 @@ export * from "./productService.service";
 export * from "./categoryService.service";
 export * from "./order.service";
 export * from "./user.service";
+export * from "./review.service";
+export * from "./inquiry.service";
