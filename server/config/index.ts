0ef07e9095c341d0b2e7ac228c997fd06a504ddc
Praveen Aththanayake <aththanayakempa@gmail.com>
2025-01-06 13:02:20 +0530

    feat: update the server config file

--- server/config/index.ts
+++ server/config/index.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-// export const MONGO_URI =
-//   process.env.MONGO_URI || "mongodb://localhost:27017/ecommerce";
-export const MONGO_URI = "mongodb://localhost:27017/ecommerce";
+export const MONGO_URI =
+  process.env.MONGO_URI || "mongodb://localhost:27017/ecommerce";
+// export const MONGO_URI = "mongodb://localhost:27017/ecommerce";
 export const APP_SECRET = process.env.APP_SECRET || "defaultsecret";
