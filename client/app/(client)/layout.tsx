d0fc9892930ac5cf1a00ce49bb58af36299f5552
Praveen Aththanayake <aththanayakempa@gmail.com>
2025-01-08 18:56:14 +0530

    feat: update the client layout.tsx

--- client/app/(client)/layout.tsx
+++ client/app/(client)/layout.tsx
@@ -1,11 +1,5 @@
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
 import Navbar from "@/components/Navbar/Navbar";
-
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700", "800", "900"],
-});
 
 export const metadata: Metadata = {
   title: "E-Commerce System",
